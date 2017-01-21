77a52b16a845b758f3454874f531e2cfc3c8af2c
Jihyeok <saurus2@naver.com>
2017-01-21 14:41:35 +0900

    jihyeok

--- week3_sort/league_jihyeok.cpp
+++ week3_sort/league_jihyeok.cpp
@@ -1,27 +1,31 @@
-#include <stdio.h>
+#include <iostream>
+#include <algorithm>
+bool op(int a,int b) { return a>b; }
+using namespace std;
 int main(){
-    int n=0,counts[10001]={0,},input=0;
-    //1 부터 10000 까지의 숫자 종류를 저장하기 위해 배열을 할당
-    scanf("%d",&n);
-    for(int i = 0; i<n; i++){
-        scanf("%d",&input);
-        counts[input]++;
-        //숫자에 인덱스를 위해 숫자의 갯수를 세어 저장
+    int n=0,a=0,b=0,sa=0,sb=0,score[101]={0,},rank[100]={0,},people=1,tscore[101]={0,};
+    cin >> n;
+    for(int i=0; i<n*(n-1)/2; i++){
+        cin >> a >> b >> sa >> sb;
+        if(sa>sb) score[a-1]+=3;
+        else if(sa<sb) score[b-1]+=3;
+        else if(sa == sb) {score[a-1]++; score[b-1]++;}
     }
-    for(int i=1; i<=10000; i++)
-        counts[i] = counts[i] + counts[i-1];
-        //정렬될 숫자의 인덱스를 위해 누적을 시킴
-    for(int i=1; i<=10000; i++){
-        int index = counts[i]-counts[i-1];
-        if(i==1){
-            index = counts[i];
-            //첫 숫자는 전의 숫자와 비교할 필요가 없음
-        }
-        while(index--){
-            printf("%d\n",i);
-            //전 숫자와 차이만큼 숫자 출력
+    for(int i=0; i<n; i++) tscore[i] = score[i];
+    sort(score,score+n,op);
+    rank[0] = 1;
+    people = 2;
+    for(int i=1; i<n; i++){
+        if(score[i] == score[i-1]) {people++; rank[i] = rank[i-1];}
+        else{rank[i] = people; people++;}
+    }
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            if(tscore[i] == score[j]) {
+                cout << rank[j] << endl;
+                break;
+            }
         }
     }
-    return 0;
+    
 }
-
--- week3_sort/treasure_jihyeok.cpp
+++ week3_sort/treasure_jihyeok.cpp
@@ -1,27 +1,23 @@
-#include <stdio.h>
+#include <iostream>
+#include <algorithm>
+using namespace std;
 int main(){
-    int n=0,counts[10001]={0,},input=0;
-    //1 부터 10000 까지의 숫자 종류를 저장하기 위해 배열을 할당
-    scanf("%d",&n);
-    for(int i = 0; i<n; i++){
-        scanf("%d",&input);
-        counts[input]++;
-        //숫자에 인덱스를 위해 숫자의 갯수를 세어 저장
-    }
-    for(int i=1; i<=10000; i++)
-        counts[i] = counts[i] + counts[i-1];
-        //정렬될 숫자의 인덱스를 위해 누적을 시킴
-    for(int i=1; i<=10000; i++){
-        int index = counts[i]-counts[i-1];
-        if(i==1){
-            index = counts[i];
-            //첫 숫자는 전의 숫자와 비교할 필요가 없음
+    int n=0,a[51]={0,},b[51]={0,},max=-1,bt[51]={0,},re=0;
+    cin >> n;
+    for(int i=0; i<n; i++) cin >> a[i];
+    for(int i=0; i<n; i++) cin >> b[i];
+    sort(a,a+n);
+    for(int i=0; i<n; i++) {
+        int idx=0;
+        max = -1;
+        for(int j=0; j<n; j++){
+            if(max<b[j] && bt[j] != 1) {
+                max=b[j];
+                idx = j;
+            }
         }
-        while(index--){
-            printf("%d\n",i);
-            //전 숫자와 차이만큼 숫자 출력
-        }
+        bt[idx]=1;
+        re+=b[idx] * a[i];
     }
-    return 0;
+    cout << re;
 }
-
