4780378f9349cef326932d8352d2319776f7adca
Jihyeok <saurus2@naver.com>
2017-01-24 19:39:42 +0900

    add explain code by jihyeok

--- week4/tree_jihyeok.cpp
+++ week4/tree_jihyeok.cpp
@@ -7,12 +7,12 @@ int cnt =0;
 void search(char a,int type){
     int cnt = 0;
     if(a == '.') return;
-    if(type == 0) cout << a;
-    search(v[a].at(cnt),type);
-    if(type == 1) cout << a;
+    if(type == 0) cout << a;//프리오더 일때 루트부터 출력
+    search(v[a].at(cnt),type);//재귀함수로 왼쪽 자식을 탐색함
+    if(type == 1) cout << a;//인오더 일때 왼쪽 자식부터 출력
     cnt++;
-    search(v[a].at(cnt),type);
-    if(type == 2) cout << a;
+    search(v[a].at(cnt),type);//오른쪽 자식 탐색
+    if(type == 2) cout << a;//포스트오더 일때 오른쪽 자식부터 출력 
     return ;
 }
 
@@ -24,7 +24,7 @@ int main(){
         cin >> a >> b >> c;
         if(ch == 0) start = a;
         ch = 1;
-        v[a].push_back(b);
+        v[a].push_back(b); //백터로 트리 생성 부모에 연결되어있는 자식들을 백터에 삽입
         v[a].push_back(c);
     }
     search(start,0);
